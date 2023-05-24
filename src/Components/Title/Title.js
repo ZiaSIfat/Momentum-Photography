d101897f3f92c04e4593726de40597f99fd4bffb
ZiaSifat <ziasifat150@gmail.com>
2023-05-24 15:48:19 +0600

    clickable logo

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Title.css';
 import logo from '../../Images/logo (1).png';
+import { Link } from 'react-router-dom';
 
 const Title = () => {
     return (
         <div>
-            <img className='title-logo' src={logo} alt="" />
+            <Link to='/'>
+                <img className='title-logo' src={logo} alt="" />
+            </Link>
             <p className='text-white'>Momentum Photography</p>
         </div>
     );
